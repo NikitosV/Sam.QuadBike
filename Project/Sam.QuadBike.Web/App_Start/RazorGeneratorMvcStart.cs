aea401095b3b2454f3a3e441c7c9c5bca81d5462
NikitosV <Niktos.V@tut.by>
2019-10-07 15:22:31 +0300

    add base Helix architecture with unicorn

---
+++ Project/Sam.QuadBike.Web/App_Start/RazorGeneratorMvcStart.cs
@@ -0,0 +1,21 @@
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using RazorGenerator.Mvc;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Sam.QuadBike.Web.RazorGeneratorMvcStart), "Start")]
+
+namespace Sam.QuadBike.Web {
+    public static class RazorGeneratorMvcStart {
+        public static void Start() {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly) {
+                UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
+            };
+
+            ViewEngines.Engines.Insert(0, engine);
+
+            // StartPage lookups are done by WebPages. 
+            VirtualPathFactoryManager.RegisterVirtualPathFactory(engine);
+        }
+    }
+}
---
+++ Project/Sam.QuadBike.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sam.QuadBike.Web.MvcApplication" Language="C#" %>
