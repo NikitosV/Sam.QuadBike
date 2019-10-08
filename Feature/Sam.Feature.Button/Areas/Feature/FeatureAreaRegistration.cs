e44915c60e35425f4359ffb93c1d547325f73fd5
NikitosV <Niktos.V@tut.by>
2019-10-08 13:29:31 +0300

    add base structure feature 'button'

---
+++ Feature/Sam.Feature.Button/Areas/Feature/FeatureAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Sam.Feature.Button.Areas.Feature
+{
+    public class FeatureAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Feature";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Feature_default",
+                "Feature/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
