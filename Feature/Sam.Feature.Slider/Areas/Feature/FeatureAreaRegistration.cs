f5a70b544f24788f3dbb5d00723b432376a41d72
NikitosV <Niktos.V@tut.by>
2019-10-09 16:02:32 +0300

    add base slider feature and fix header.cshtml

---
+++ Feature/Sam.Feature.Slider/Areas/Feature/FeatureAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Sam.Feature.Slider.Areas.Feature
+{
+    public class FeatureAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Feature";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Feature_default",
+                "Feature/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
