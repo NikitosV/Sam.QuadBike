50803556effeb797292d980d33677d74b3e94ba7
NikitosV <Niktos.V@tut.by>
2019-10-15 16:18:46 +0300

    add mechanism of DI in Foundation

--- Foundation/Sam.Foundation.DependencyInjection/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sam.Foundation.DependencyInjection.MvcApplication" Language="C#" %>
---
+++ Foundation/Sam.Foundation.DependencyInjection/Infrastructure/MvcControllerServicesConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Sam.Foundation.DependencyInjection.Infrastructure
+{
+    public class MvcControllerServicesConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMvcControllers("*.Feature.*");
+            serviceCollection.AddClassesWithServiceAttribute("*.Feature.*");
+
+            serviceCollection.AddMvcControllers("*.Foundation.*");
+            serviceCollection.AddClassesWithServiceAttribute("*.Foundation.*");
+
+            serviceCollection.AddMvcControllers("*.QuadBike.*");
+            serviceCollection.AddClassesWithServiceAttribute("*.ToolStockSc.*");
+        }
+    }
+}
