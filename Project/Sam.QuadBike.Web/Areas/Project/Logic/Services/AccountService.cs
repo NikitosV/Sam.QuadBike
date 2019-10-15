c647e703cf1a50e6b967d0bc1a28ba82868f1618
NikitosV <Niktos.V@tut.by>
2019-10-15 16:39:07 +0300

    add base account logic

---
+++ Project/Sam.QuadBike.Web/Areas/Project/Logic/Services/AccountService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sam.QuadBike.Web.Areas.Project.Logic.Services
+{
+    public class AccountService
+    {
+    }
+}
---
+++ Project/Sam.QuadBike.Web/Areas/Project/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sam.QuadBike.Web.Areas.Project.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+    }
+}
